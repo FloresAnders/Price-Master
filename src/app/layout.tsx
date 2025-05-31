5fa20bbeeecca25f633b585dbf08759211a407d1
FloresAnders <anders.flores.morales@est.una.ac.cr>
2025-05-31 13:52:55 -0600

    [REMOVE]generateThemeColor and generateViewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,28 +14,13 @@ export const metadata = {
   // Removed viewport and themeColor from metadata
 };
 
-// Generar la etiqueta <meta name="viewport">
-export function generateViewport(): string {
-  return 'width=device-width, initial-scale=1';
-}
-
-// Generar la etiqueta <meta name="theme-color">
-export function generateThemeColor() {
-  return [
-    { media: '(prefers-color-scheme: light)', color: 'white' },
-    { media: '(prefers-color-scheme: dark)', color: 'black' },
-  ];
-}
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className={`${inter.className} min-h-screen`}>
-            <main>
-              {children}
-            </main>
+            <main>{children}</main>
           </div>
         </ThemeProvider>
       </body>
