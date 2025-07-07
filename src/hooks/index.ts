136984a32a0678081b1fe6ada8363c84309e8002
FloresAnders <126523827+FloresAnders@users.noreply.github.com>
2025-07-07 17:40:52 -0600

    Add barrel file for hooks exports
    Created src/hooks/index.ts to re-export all custom hooks for easier and cleaner imports throughout the project.

---
+++ src/hooks/index.ts
@@ -0,0 +1,7 @@
+export { useAuth } from './useAuth';
+export { useBarcodeScanner } from './useBarcodeScanner';
+export { useChatMessages } from './useChatMessages';
+export { useChatUsers } from './useChatUsers';
+export { useRouteProtection } from './useRouteProtection';
+export { useScanning } from './useScanning';
+export { useSchedules } from './useSchedules';
